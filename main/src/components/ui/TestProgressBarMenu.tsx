c0293a8e216fd0c6b0f0668f54d663e2556c8396
Marcus Hobdy <hobdy.marcusjr@gmail.com>
2022-12-07 10:24:18 -0700

    Feat: updated logic for open close, switch from pop over to toast, added additional style logic

--- main/src/components/ui/TestProgressBarMenu.tsx
+++ main/src/components/ui/TestProgressBarMenu.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import {
 	Box,
 	Button,
 	HStack,
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
 	Progress,
 	Text,
 	VStack,
+	useToast,
 } from '@chakra-ui/react';
 import AmpMicroChip from '../../css/AmpMicroChip';
 import { useTranslation } from 'react-i18next';
@@ -18,6 +16,8 @@ import ProgressMessageComponent from './ProgressMessageComponent';
 const TestProgressBarMenu = () => {
 	const { t: i18n } = useTranslation();
 	const [isOpen, setIsOpen] = useState(false);
+	const toast = useToast();
+	const id = 'progress-toast';
 
 	return (
 		<Box width="100vw" boxSizing="border-box">
@@ -54,6 +54,7 @@ const TestProgressBarMenu = () => {
 						<AmpMicroChip variant="ampNeutralUnfilled" />
 					</HStack>
 				</VStack>
+<<<<<<< HEAD
 				<Popover>
 					<PopoverTrigger>
 						<Button
@@ -83,6 +84,69 @@ const TestProgressBarMenu = () => {
 						</Button>
 					</PopoverContent>
 				</Popover>
+=======
+				<Button
+					variant={'outline'}
+					borderColor={'ampPrimary.300'}
+					bg="ampWhite"
+					width="200px"
+					leftIcon={isOpen ? <ExitIcon /> : <EnterIcon />}
+					onClick={() => {
+						if (!toast.isActive(id)) {
+							toast({
+								position: 'top-right',
+								duration: 10000,
+								isClosable: true,
+								id,
+								render: () => (
+									<Box
+										marginTop={'160px'}
+										padding={'24px'}
+										paddingRight={'400px'}
+										borderRadius={'24px'}
+										width={'375px'}
+										marginLeft="200px"
+										bg="ampWhite">
+										<Box
+											width="325px"
+											bg="ampSuccess.50"
+											borderRadius={'12px'}
+											padding="20px">
+											<HStack>
+												<AvatarGroup spacing="4px">
+													<Avatar bg="teal.500" />
+												</AvatarGroup>
+												<Text fontSize={'16px'}>
+													You’re doing a great job at knowing what you are sure
+													and unsure about.
+												</Text>
+											</HStack>
+										</Box>
+										<Button
+											variant={'outline'}
+											borderColor={'ampPrimary.300'}
+											bg="ampWhite"
+											width="325px"
+											height="40px"
+											marginTop="24px"
+											onClick={() => {
+												setIsOpen(!isOpen);
+												toast.close(id);
+											}}>
+											<Text fontSize={'16px'} fontWeight="600">
+												{i18n('viewMoreProgress')}
+											</Text>
+										</Button>
+									</Box>
+								),
+							});
+						}
+					}}>
+					<Text fontSize={'16px'} fontWeight="600">
+						{isOpen ? i18n('hideProgress') : i18n('showProgress')}
+					</Text>
+				</Button>
+>>>>>>> 4f964410d (Feat: updated logic for open close, switch from pop over to toast, added additional style logic)
 			</HStack>
 			<Progress
 				value={80}
