d93575618e8fd47aa1addb67d36684e38de56658
Jason Watkins <jwatkins@amplifire.com>
2022-12-08 14:59:46 -0500

    new register service integration

--- src/components/self-registration/Register.jsx
+++ src/components/self-registration/Register.jsx
@@ -3,11 +3,11 @@ import { Link as ReactRouterLink, useOutletContext } from 'react-router-dom';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { useTranslation } from 'react-i18next';
 import useRegisterService from '../../services/useRegisterService';
-import useSignupDataService from '../../services/useSignupDataService';
 import {
 	Alert,
 	AlertIcon,
 	Button,
+	Center,
 	FormControl,
 	FormErrorMessage,
 	FormLabel,
@@ -22,27 +22,12 @@ import {
 } from '@chakra-ui/react';
 
 function Register() {
-	const { t: i18n } = useTranslation();
 	const context = useOutletContext();
-	const [step, setStep] = useState(1);
-	const [verified, setVerified] = useState(false);
-	const [recaptchaRes, setRecaptchaRes] = useState(null);
-	const [title, setTitle] = useState('');
-	const [errorMessage, setErrorMessage] = useState('');
-	const [userAltKey, setUserAltKey] = useState('');
+	const { t: i18n } = useTranslation();
 	const recaptchaRef = useRef();
-	const [formError, setFormError] = useState({
-		firstName: false,
-		lastName: false,
-		emailAddress: false,
-		userName: false,
-		password: false,
-		confirmPassword: false,
-	});
-
-	const { postSignupData } = useSignupDataService();
-
 	const { postPersonalDetails } = useRegisterService();
+
+	const [errorMessage, setErrorMessage] = useState('');
 	const [formData, setFormData] = useState({
 		firstName: '',
 		lastName: '',
@@ -51,12 +36,46 @@ function Register() {
 		password: '',
 		confirmPassword: '',
 	});
+	const [formError, setFormError] = useState({
+		firstName: false,
+		lastName: false,
+		emailAddress: false,
+		userName: false,
+		password: false,
+		confirmPassword: false,
+	});
+	const [recaptchaRes, setRecaptchaRes] = useState(null);
+	const [showForm, setShowForm] = useState(true);
+	const [title, setTitle] = useState('');
+	const [verified, setVerified] = useState(false);
+
+	const checkFormErrors = () => {
+		for (let errorState in formError) {
+			if (formError[errorState] === true) {
+				return true;
+			}
+		}
+		return false;
+	};
 
 	const onRecaptchaChange = (value) => {
 		if (value) {
 			setRecaptchaRes(value);
 			setVerified(true);
+		} else {
+			setVerified(false);
 		}
+	};
+
+	const clearFormData = () => {
+		setFormData({
+			firstName: '',
+			lastName: '',
+			emailAddress: '',
+			userName: '',
+			password: '',
+			confirmPassword: '',
+		});
 	};
 
 	useEffect(() => {
@@ -120,11 +139,23 @@ function Register() {
 		}
 	};
 
+	const showConfirmation = () => {
+		setShowForm(false);
+	};
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		setErrorMessage('');
 
-		if (verified && !formError) {
+		if (!verified) {
+			setErrorMessage(i18n('pleaseCompleteRecaptcha'));
+		}
+
+		if (
+			verified &&
+			checkFormErrors() === false &&
+			formData.password === formData.confirmPassword
+		) {
 			const personalDetailsResponse = await postPersonalDetails(
 				formData,
 				context.accountUid,
@@ -132,192 +163,195 @@ function Register() {
 				recaptchaRes,
 			);
 
-			if (!personalDetailsResponse.ok) {
+			if (personalDetailsResponse?.data?.items) {
 				setErrorMessage(i18n('unknown error'));
 				throw new Error(`Error! status: ${personalDetailsResponse.status}`);
 			}
 
-			personalDetailsResponse.json().then((data) => {
-				setUserAltKey(data.key);
-				setTitle(i18n('signUpText'));
-				// remove recaptcha in dom
-				document.getElementById('recaptcha').style.display = 'none';
-			});
-
-			const signupResponse = await postSignupData(
-				context.accountUid,
-				userAltKey,
-				formData.userName,
-				formData.password,
-				recaptchaRes,
-			);
-
-			if (!signupResponse.ok) {
-				setErrorMessage(i18n('unknownError'));
-				throw new Error(`Error! status: ${signupResponse.status}`);
+			if (personalDetailsResponse.status === 201) {
+				showConfirmation();
 			}
-
-			setStep(step + 1);
-			setTitle('');
-			/////////////////////// end step 2
-		} else {
-			return setErrorMessage(i18n('pleaseCompleteRecaptcha'));
+			clearFormData();
 		}
 	};
 
 	return (
 		<>
-			<VStack spacing={5} as="form" onSubmit={handleSubmit}>
-				<Heading>{i18n(title)}</Heading>
+			{showForm && (
+				<VStack spacing={5} as="form" onSubmit={handleSubmit}>
+					<Heading>{i18n(title)}</Heading>
 
-				<HStack spacing={5}>
+					<HStack spacing={5}>
+						<VStack>
+							<FormControl isRequired isInvalid={formError.firstName}>
+								<FormLabel marginBottom={1} requiredIndicator>
+									{i18n('first name')}
+								</FormLabel>
+								<Input
+									id="firstName"
+									placeholder="first name"
+									name="firstName"
+									onChange={handleChange('firstName')}
+									value={formData.firstName}
+									onBlur={validateInput}
+								/>
+								<FormErrorMessage>{i18n('enterFirstName')}</FormErrorMessage>
+							</FormControl>
+
+							<FormControl isRequired isInvalid={formError.lastName}>
+								<FormLabel marginBottom={1} requiredIndicator>
+									{i18n('last name')}
+								</FormLabel>
+								<Input
+									id="lastName"
+									placeholder="last name"
+									name="lastName"
+									onChange={handleChange('lastName')}
+									value={formData.lastName}
+									onBlur={validateInput}
+								/>
+								<FormErrorMessage>{i18n('enterLastName')}</FormErrorMessage>
+							</FormControl>
+
+							<FormControl isRequired isInvalid={formError.emailAddress}>
+								<FormLabel marginBottom={1} requiredIndicator>
+									{i18n('email')}
+								</FormLabel>
+								<Input
+									id="emailAddress"
+									placeholder="name@email.com"
+									name="emailAddress"
+									onChange={handleChange('emailAddress')}
+									value={formData.emailAddress}
+									onBlur={validateInput}
+								/>
+								<FormErrorMessage>{i18n('enterEmailAddress')}</FormErrorMessage>
+							</FormControl>
+						</VStack>
+						<VStack>
+							<FormControl isRequired isInvalid={formError.userName}>
+								<FormLabel marginBottom={1} requiredIndicator>
+									{i18n('username')}
+								</FormLabel>
+								<Input
+									id="userName"
+									placeholder="name@email.com"
+									name="userName"
+									onChange={handleChange('userName')}
+									value={formData.userName}
+									onBlur={validateInput}
+								/>
+								<FormErrorMessage>{i18n('enterUsername')}</FormErrorMessage>
+							</FormControl>
+
+							<FormControl isRequired isInvalid={formError.password}>
+								<FormLabel marginBottom={1} requiredIndicator>
+									{i18n('password')}
+								</FormLabel>
+								<Input
+									id="password"
+									placeholder="password"
+									type="password"
+									name="password"
+									onChange={handleChange('password')}
+									value={formData.password}
+									onBlur={validatePassword}
+								/>
+								<FormErrorMessage>{i18n('enterPassword')}</FormErrorMessage>
+							</FormControl>
+
+							<FormControl isRequired isInvalid={formError.password}>
+								<FormLabel marginBottom={1} requiredIndicator>
+									{i18n('confirmPassword')}
+								</FormLabel>
+								<Input
+									id="confirmPassword"
+									placeholder="confirm password"
+									type="password"
+									name="confirmPassword"
+									onChange={handleChange('confirmPassword')}
+									value={formData.confirmPassword}
+									onBlur={validateConfirmPassword}
+								/>
+								<FormErrorMessage>{i18n('enterPassword')}</FormErrorMessage>
+							</FormControl>
+						</VStack>
+					</HStack>
+
+					{context.recaptcha && (
+						<ReCAPTCHA
+							id="recaptcha"
+							ref={recaptchaRef}
+							sitekey={context.recaptcha}
+							onChange={onRecaptchaChange}
+						/>
+					)}
+
+					<Button onClick={handleSubmit} type="submit" name="Login">
+						{i18n('submitBtnText')}
+					</Button>
+
+					{errorMessage && (
+						<Alert status="error" bg="ampError.50">
+							<AlertIcon />
+							<Text align="center" color="ampError.700">
+								{errorMessage}
+							</Text>
+						</Alert>
+					)}
+
+					<Text>
+						{i18n('passwordRequirements', {
+							charactersNumber: 5,
+							numberOfCriteria: 3,
+						})}
+					</Text>
+
+					<UnorderedList>
+						<ListItem>{i18n('upperCaseRule')}</ListItem>
+						<ListItem>{i18n('lowerCaseRule')}</ListItem>
+						<ListItem>{i18n('digitRule')}</ListItem>
+						<ListItem>{i18n('specialCharacterRule')}</ListItem>
+					</UnorderedList>
+
+					<Text>
+						If you are already registered,{' '}
+						<Link
+							as={ReactRouterLink}
+							to={{
+								pathname: '/login',
+								search: `?abbrevName=${context.abbrevNameState}`,
+							}}
+							color="ampSecondary.500"
+							textDecoration="underline">
+							{i18n('clickHere')}
+						</Link>{' '}
+						{i18n('toLogIn')}
+					</Text>
+				</VStack>
+			)}
+
+			{!showForm && (
+				<Center height="100%">
 					<VStack>
-						<FormControl isRequired isInvalid={formError.firstName}>
-							<FormLabel marginBottom={1} requiredIndicator>
-								{i18n('first name')}
-							</FormLabel>
-							<Input
-								id="firstName"
-								placeholder="first name"
-								name="firstName"
-								onChange={handleChange('firstName')}
-								value={formData.firstName}
-								onBlur={validateInput}
-							/>
-							<FormErrorMessage>{i18n('enterFirstName')}</FormErrorMessage>
-						</FormControl>
-
-						<FormControl isRequired isInvalid={formError.lastName}>
-							<FormLabel marginBottom={1} requiredIndicator>
-								{i18n('last name')}
-							</FormLabel>
-							<Input
-								id="lastName"
-								placeholder="last name"
-								name="lastName"
-								onChange={handleChange('lastName')}
-								value={formData.lastName}
-								onBlur={validateInput}
-							/>
-							<FormErrorMessage>{i18n('enterLastName')}</FormErrorMessage>
-						</FormControl>
-
-						<FormControl isRequired isInvalid={formError.emailAddress}>
-							<FormLabel marginBottom={1} requiredIndicator>
-								{i18n('email')}
-							</FormLabel>
-							<Input
-								id="emailAddress"
-								placeholder="name@email.com"
-								name="emailAddress"
-								onChange={handleChange('emailAddress')}
-								value={formData.emailAddress}
-								onBlur={validateInput}
-							/>
-							<FormErrorMessage>{i18n('enterEmailAddress')}</FormErrorMessage>
-						</FormControl>
-					</VStack>
-					<VStack>
-						<FormControl isRequired isInvalid={formError.userName}>
-							<FormLabel marginBottom={1} requiredIndicator>
-								{i18n('username')}
-							</FormLabel>
-							<Input
-								id="userName"
-								placeholder="name@email.com"
-								name="userName"
-								onChange={handleChange('userName')}
-								value={formData.userName}
-								onBlur={validateInput}
-							/>
-							<FormErrorMessage>{i18n('enterUsername')}</FormErrorMessage>
-						</FormControl>
-
-						<FormControl isRequired isInvalid={formError.password}>
-							<FormLabel marginBottom={1} requiredIndicator>
-								{i18n('password')}
-							</FormLabel>
-							<Input
-								id="password"
-								placeholder="password"
-								type="password"
-								name="password"
-								onChange={handleChange('password')}
-								value={formData.password}
-								onBlur={validatePassword}
-							/>
-							<FormErrorMessage>{i18n('enterPassword')}</FormErrorMessage>
-						</FormControl>
-
-						<FormControl isRequired isInvalid={formError.password}>
-							<FormLabel marginBottom={1} requiredIndicator>
-								{i18n('confirmPassword')}
-							</FormLabel>
-							<Input
-								id="confirmPassword"
-								placeholder="confirm password"
-								type="password"
-								name="confirmPassword"
-								onChange={handleChange('confirmPassword')}
-								value={formData.confirmPassword}
-								onBlur={validateConfirmPassword}
-							/>
-							<FormErrorMessage>{i18n('enterPassword')}</FormErrorMessage>
-						</FormControl>
-					</VStack>
-				</HStack>
-
-				{context.recaptcha && (
-					<ReCAPTCHA
-						id="recaptcha"
-						ref={recaptchaRef}
-						sitekey={context.recaptcha}
-						onChange={onRecaptchaChange}
-					/>
-				)}
-
-				<Button onClick={handleSubmit} type="submit" name="Login">
-					{step === 1 ? i18n('continueBtnText') : i18n('submitBtnText')}
-				</Button>
-
-				{errorMessage && (
-					<Alert status="error" bg="ampError.50">
-						<AlertIcon />
-						<Text align="center" color="ampError.700">
-							{errorMessage}
+						<Text>{i18n('userCreated')}</Text>
+						<Text>
+							{i18n('please')}{' '}
+							<Link
+								as={ReactRouterLink}
+								to={{
+									pathname: '/login',
+									search: `?abbrevName=${context.abbrevNameState}`,
+								}}
+								color="ampSecondary.500"
+								textDecoration="underline">
+								{i18n('clickHere')}
+							</Link>{' '}
+							{i18n('toLogIn')}
 						</Text>
-					</Alert>
-				)}
-
-				<Text>
-					{i18n('passwordRequirements', {
-						charactersNumber: 5,
-						numberOfCriteria: 3,
-					})}
-				</Text>
-
-				<UnorderedList>
-					<ListItem>{i18n('upperCaseRule')}</ListItem>
-					<ListItem>{i18n('lowerCaseRule')}</ListItem>
-					<ListItem>{i18n('digitRule')}</ListItem>
-					<ListItem>{i18n('specialCharacterRule')}</ListItem>
-				</UnorderedList>
-
-				<Text>
-					If you are already registered,{' '}
-					<Link
-						as={ReactRouterLink}
-						to={{
-							pathname: '/login',
-							search: `?abbrevName=${context.abbrevNameState}`,
-						}}
-						color="ampSecondary.500"
-						textDecoration="underline">
-						{i18n('clickHere')}
-					</Link>{' '}
-					{i18n('toLogIn')}
-				</Text>
-			</VStack>
+					</VStack>
+				</Center>
+			)}
 		</>
 	);
 }
--- src/services/useRegisterService.js
+++ src/services/useRegisterService.js
@@ -8,7 +8,7 @@ const useRegisterService = () => {
 	const [loading, setLoading] = useState(false);
 	const { setShowAlert } = useContext(DialogContext);
 	const postPersonalDetails = async (
-		{ firstName, lastName, emailAddress },
+		{ emailAddress, firstName, lastName, password, userName },
 		accountUid,
 		subAcccount,
 		captchaResponse,
@@ -16,46 +16,47 @@ const useRegisterService = () => {
 		try {
 			setLoading(true);
 			const personalDetailsBody = {
-				userName: null,
-				password: null,
-				ssoId: null,
-				roleKeys: null,
-				roleNames: null,
-				createdTimestamp: null,
-				modifiedTimestamp: null,
-				customAttributes: null,
-				xid: null,
+				emailAddress,
 				firstName,
 				lastName,
-				emailAddress,
-				isActive: true,
-				key: null,
-				uid: null,
-				changeHistory: null,
-				self: null,
+				password,
+				userName,
 			};
 
 			return await axios({
+<<<<<<< HEAD:src/services/useRegisterService.js
 <<<<<<< HEAD:src/services/useRegisterService.js
 				url: `${window.KF.state.baseUri}/v2/self-registration/${accountAltKey}?captchaResponse=${captchaResponse}`,
 				headers: { 'Content-Type': 'application/json' },
 =======
 				url: `/v2/self-registration/${accountUid}?captchaResponse=${captchaResponse}&subaccount=${subAcccount}`,
+=======
+				method: 'POST',
+				url: `/v2/self-registration/${accountUid}?captchaResponse=${captchaResponse}&subaccount=${subAcccount}&createCredentials=true`,
+				data: personalDetailsBody,
+>>>>>>> 89fce0cd1 (new register service integration):main/src/services/useRegisterService.js
 				headers: {
 					'Content-Type': 'application/json',
 					Accept: 'application/json',
 				},
+<<<<<<< HEAD:src/services/useRegisterService.js
 >>>>>>> fdfeea350 (modified personDetailsBody for Rest call):main/src/services/useRegisterService.js
 				body: personalDetailsBody,
 				dataType: 'json',
 				method: 'POST',
+=======
+>>>>>>> 89fce0cd1 (new register service integration):main/src/services/useRegisterService.js
 			});
 		} catch (err) {
 			console.log(err);
 			setError(err);
+<<<<<<< HEAD:src/services/useRegisterService.js
 			if (err.response.status >= 500) {
 				setShowAlert(true);
 			}
+=======
+			return err;
+>>>>>>> 89fce0cd1 (new register service integration):main/src/services/useRegisterService.js
 		} finally {
 			setLoading(false);
 		}
