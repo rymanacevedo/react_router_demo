3fd029e83ab731f0bf4b0235fcffc63e9bc66fd1
Jason Watkins <jwatkins@amplifire.com>
2023-01-23 13:25:15 -0500

    moved i18n mock setup to setupTests.ts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { render, screen } from '@testing-library/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ampTheme } from './css/theme';
-
 import { MemoryRouter } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import DialogProvider from './components/DialogProvider';
 import App from './App';
-
-jest.mock('react-i18next');
-
-beforeEach(() => {
-	useTranslation.mockReturnValue({ t: (key: any) => key });
-});
 
 test('renders welcomeMessage', async () => {
 	render(
