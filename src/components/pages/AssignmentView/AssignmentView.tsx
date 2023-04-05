c8fb43c550a2e6d13eb594ac37111e768dcd4e8e
Ryan Acevedo <racevedo@amplifire.com>
2023-04-05 11:35:25 -0600

    added suspense component into view and layout for Loading

--- src/components/pages/AssignmentView/AssignmentView.tsx
+++ src/components/pages/AssignmentView/AssignmentView.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import {
 	Box,
 	Container,
@@ -7,7 +7,6 @@ import {
 	ModalOverlay,
 	useMediaQuery,
 } from '@chakra-ui/react';
-import TestProgressBarMenu from '../../ui/TestProgressBarMenu';
 import ProgressMenu from '../../ui/ProgressMenu';
 import Question from '../../ui/Question';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -25,6 +24,8 @@ import { useLocalStorage } from '../../../hooks/useLocalStorage';
 import { findDateData } from '../../../utils/logic';
 import { Cookies } from 'react-cookie-consent';
 import AnswerArea from '../../ui/AnswerInput/AnswerArea';
+import TestProgressBarMenu from '../../ui/TestProgressBarMenu';
+import LoadingAssignmentView from '../../ui/loading/LoadingAssignmentView';
 
 const initState = {
 	self: null,
@@ -50,6 +51,8 @@ const initState = {
 	avatarMessage: null,
 	answerList: [],
 };
+
+// const TestProgressBarMenu = lazy(() => import('../../ui/TestProgressBarMenu'));
 
 const AssignmentView = () => {
 	const [isSmallerThan1000] = useMediaQuery('(max-width: 1000px)');
@@ -262,66 +265,72 @@ const AssignmentView = () => {
 			<Modal isOpen={isInstructionalOverlayOpen} onClose={onClose}>
 				<ModalOverlay />
 			</Modal>
-			<Container
-				id={'learning-assignment'}
-				margin="0"
-				padding="0"
-				maxWidth={'100vw'}
-				overflowY={'hidden'}
-				overflowX={'hidden'}>
-				<TestProgressBarMenu
-					questionData={questionData}
-					isMenuOpen={isMenuOpen}
-					setIsMenuOpen={setIsMenuOpen}
-					currentRoundQuestionListData={currentRoundQuestionListData}
-					currentQuestion={questionInFocus}
-					currentRoundAnswerOverLayData={currentRoundAnswerOverLayData}
-				/>{' '}
-				<HStack width="100%">
-					<HStack
-						w="100%"
-						p="12px"
-						justifyContent={'center'}
-						flexWrap={isSmallerThan1000 ? 'wrap' : 'nowrap'}>
-						<Box
-							style={{
-								backgroundColor: 'white',
-								margin: '6px',
-							}}
-							boxShadow="2xl"
-							w="100%"
-							maxWidth={726}
-							h={isSmallerThan1000 ? '' : '745px'}
-							overflow="hidden"
-							borderRadius={24}
-							p={'72px'}>
-							<Question questionInFocus={questionInFocus} />
-						</Box>
-						<AnswerArea
-							isOpen={isInstructionalOverlayOpen}
-							onClose={onClose}
-							smallerThan1000={isSmallerThan1000}
-							initialFocusRef={initRef}
-							showOverlay={showOverlay}
-							questionInFocus={questionInFocus}
-							selectedAnswers={selectedAnswers}
-							selectedAnswersState={setSelectedAnswers}
-							clearSelection={clearSelection}
-							clearSelectionState={setClearSelection}
-							currentRoundAnswerOverLayData={currentRoundAnswerOverLayData}
-							onClick={continueBtnFunc}
-							clearSelectionFunction={clearSelectionButtonFunc}
-							IDKResponse={IDKResponse}
-							setIDKResponse={setIDKResponse}
-						/>
-					</HStack>
-					<ProgressMenu
+			<LoadingAssignmentView isSmallerThan1000={isSmallerThan1000} />
+			<Suspense
+				fallback={
+					<LoadingAssignmentView isSmallerThan1000={isSmallerThan1000} />
+				}>
+				<Container
+					id={'learning-assignment'}
+					margin="0"
+					padding="0"
+					maxWidth={'100vw'}
+					overflowY={'hidden'}
+					overflowX={'hidden'}>
+					<TestProgressBarMenu
+						questionData={questionData}
 						isMenuOpen={isMenuOpen}
+						setIsMenuOpen={setIsMenuOpen}
 						currentRoundQuestionListData={currentRoundQuestionListData}
+						currentQuestion={questionInFocus}
 						currentRoundAnswerOverLayData={currentRoundAnswerOverLayData}
 					/>
-				</HStack>
-			</Container>
+					<HStack width="100%">
+						<HStack
+							w="100%"
+							p="12px"
+							justifyContent={'center'}
+							flexWrap={isSmallerThan1000 ? 'wrap' : 'nowrap'}>
+							<Box
+								style={{
+									backgroundColor: 'white',
+									margin: '6px',
+								}}
+								boxShadow="2xl"
+								w="100%"
+								maxWidth={726}
+								h={isSmallerThan1000 ? '' : '745px'}
+								overflow="hidden"
+								borderRadius={24}
+								p={'72px'}>
+								<Question questionInFocus={questionInFocus} />
+							</Box>
+							<AnswerArea
+								isOpen={isInstructionalOverlayOpen}
+								onClose={onClose}
+								smallerThan1000={isSmallerThan1000}
+								initialFocusRef={initRef}
+								showOverlay={showOverlay}
+								questionInFocus={questionInFocus}
+								selectedAnswers={selectedAnswers}
+								selectedAnswersState={setSelectedAnswers}
+								clearSelection={clearSelection}
+								clearSelectionState={setClearSelection}
+								currentRoundAnswerOverLayData={currentRoundAnswerOverLayData}
+								onClick={continueBtnFunc}
+								clearSelectionFunction={clearSelectionButtonFunc}
+								IDKResponse={IDKResponse}
+								setIDKResponse={setIDKResponse}
+							/>
+						</HStack>
+						<ProgressMenu
+							isMenuOpen={isMenuOpen}
+							currentRoundQuestionListData={currentRoundQuestionListData}
+							currentRoundAnswerOverLayData={currentRoundAnswerOverLayData}
+						/>
+					</HStack>
+				</Container>
+			</Suspense>
 		</main>
 	);
 };
---
+++ src/components/ui/loading/LoadingAssignmentView.tsx
@@ -0,0 +1,96 @@
+import {
+	Box,
+	Container,
+	HStack,
+	Skeleton,
+	SkeletonCircle,
+	SkeletonText,
+	Stack,
+} from '@chakra-ui/react';
+
+type Props = {
+	isSmallerThan1000: boolean;
+};
+export default function LoadingAssignmentView({ isSmallerThan1000 }: Props) {
+	return (
+		<Container
+			id={'loading'}
+			margin="0"
+			padding="0"
+			maxWidth={'100vw'}
+			overflowY={'hidden'}
+			overflowX={'hidden'}>
+			<Stack
+				padding={5}
+				borderBottom={'1px'}
+				borderBottomColor="ampSecondary.300">
+				<Skeleton height="20px" width={'235px'} />
+				<Skeleton height="20px" width={'110px'} />
+			</Stack>
+			<HStack width="100%">
+				<HStack
+					w="100%"
+					p="12px"
+					justifyContent={'center'}
+					flexWrap={isSmallerThan1000 ? 'wrap' : 'nowrap'}>
+					<Box
+						style={{
+							backgroundColor: 'white',
+							margin: '6px',
+						}}
+						boxShadow="2xl"
+						w="100%"
+						maxWidth={726}
+						h={isSmallerThan1000 ? '' : '745px'}
+						overflow="hidden"
+						borderRadius={24}
+						p={'72px'}>
+						<Skeleton height="20px" width={'235px'} />
+						<SkeletonText mt="4" noOfLines={4} spacing="4" skeletonHeight="2" />
+					</Box>
+					<Box
+						style={{
+							backgroundColor: 'white',
+							margin: '6px',
+							minHeight: '745px',
+						}}
+						boxShadow="2xl"
+						h={isSmallerThan1000 ? '' : '100%'}
+						display={'flex'}
+						flexDirection="column"
+						w="100%"
+						maxWidth={726}
+						overflow="hidden"
+						borderRadius={24}
+						p={'72px'}>
+						<Skeleton height="20px" width={'235px'} />
+						<HStack marginTop={5}>
+							<SkeletonCircle size="10" />
+							<Box style={{ flex: 1 }}>
+								<Skeleton height="20px" width={'250px'} />
+							</Box>
+						</HStack>
+						<HStack marginTop={5}>
+							<SkeletonCircle size="10" />
+							<Box style={{ flex: 1 }}>
+								<Skeleton height="20px" width={'250px'} />
+							</Box>
+						</HStack>
+						<HStack marginTop={5}>
+							<SkeletonCircle size="10" />
+							<Box style={{ flex: 1 }}>
+								<Skeleton height="20px" width={'250px'} />
+							</Box>
+						</HStack>
+						<HStack marginTop={5}>
+							<SkeletonCircle size="10" />
+							<Box style={{ flex: 1 }}>
+								<Skeleton height="20px" width={'250px'} />
+							</Box>
+						</HStack>
+					</Box>
+				</HStack>
+			</HStack>
+		</Container>
+	);
+}
