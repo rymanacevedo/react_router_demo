1bc0f4384a06e74b7f73280191e02b6bee6e2403
Kush Shah <kshah@amplifire.com>
2023-04-20 13:43:14 -0600

    add useCallback logic and logic to reset for next round

--- src/hooks/useQuizContext.tsx
+++ src/hooks/useQuizContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useMemo, useState } from 'react';
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useMemo,
+	useState,
+} from 'react';
 
 type QuizContextType = {
 	message: {
@@ -43,138 +49,141 @@ export const QuizProvider = ({ children }: { children: any }) => {
 
 	const [selectedCourseKey, setSelectedCourseKey] = useState('');
 
-	const handleMessage = (messageType: string, reset: boolean) => {
-		const resetFiveFastAnswers = () => {
-			setMessage({
-				...message,
-				FIVE_FAST_ANSWERS: 0,
-			});
-		};
+	const handleMessage = useCallback(
+		(messageType: string, reset: boolean) => {
+			const resetFiveFastAnswers = () => {
+				setMessage({
+					...message,
+					FIVE_FAST_ANSWERS: 0,
+				});
+			};
 
-		const resetFiveConsecutiveSureCorrect = () => {
-			setMessage({
-				...message,
-				FIVE_CONSEC_SC: 0,
-			});
-		};
+			const resetFiveConsecutiveSureCorrect = () => {
+				setMessage({
+					...message,
+					FIVE_CONSEC_SC: 0,
+				});
+			};
 
-		const resetFiveConsecutiveSureIncorrect = () => {
-			setMessage({
-				...message,
-				FIVE_CONSEC_SI: 0,
-			});
-		};
+			const resetFiveConsecutiveSureIncorrect = () => {
+				setMessage({
+					...message,
+					FIVE_CONSEC_SI: 0,
+				});
+			};
 
-		const resetSixDontKnowInRound = () => {
-			setMessage({
-				...message,
-				SIX_DK_IN_ROUND: 0,
-			});
-		};
+			const resetSixDontKnowInRound = () => {
+				setMessage({
+					...message,
+					SIX_DK_IN_ROUND: 0,
+				});
+			};
 
-		const resetFullRoundOfSureCorrect = () => {
-			setMessage({
-				...message,
-				FULL_ROUND_OF_SC: 0,
-			});
-		};
+			const resetFullRoundOfSureCorrect = () => {
+				setMessage({
+					...message,
+					FULL_ROUND_OF_SC: 0,
+				});
+			};
 
-		const resetFiveFastReviews = () => {
-			setMessage({
-				...message,
-				FIVE_FAST_REVIEWS: 0,
-			});
-		};
-		const resetTenLongReviews = () => {
-			setMessage({
-				...message,
-				TEN_LONG_REVIEWS: 0,
-			});
-		};
+			const resetFiveFastReviews = () => {
+				setMessage({
+					...message,
+					FIVE_FAST_REVIEWS: 0,
+				});
+			};
+			const resetTenLongReviews = () => {
+				setMessage({
+					...message,
+					TEN_LONG_REVIEWS: 0,
+				});
+			};
 
-		switch (messageType) {
-			case 'FIVE_FAST_ANSWERS':
-				if (reset || message.FIVE_FAST_ANSWERS === 5) {
-					resetFiveFastAnswers();
-				} else {
-					setMessage({
-						...message,
-						FIVE_FAST_ANSWERS: message.FIVE_FAST_ANSWERS + 1,
-					});
-				}
-				break;
-			case 'FIVE_CONSEC_SI':
-				if (reset || message.FIVE_CONSEC_SI === 5) {
-					resetFiveConsecutiveSureIncorrect();
-				} else {
-					setMessage({
-						...message,
-						FIVE_CONSEC_SI: message.FIVE_CONSEC_SI + 1,
-					});
-				}
-				break;
-			case 'SIX_DK_IN_ROUND':
-				if (reset || message.SIX_DK_IN_ROUND === 6) {
-					resetSixDontKnowInRound();
-				} else {
-					setMessage({
-						...message,
-						SIX_DK_IN_ROUND: message.SIX_DK_IN_ROUND + 1,
-					});
-				}
-				break;
-			case 'FIVE_CONSEC_SC':
-				if (reset || message.FIVE_CONSEC_SC === 5) {
-					resetFiveConsecutiveSureCorrect();
-				} else {
-					setMessage({
-						...message,
-						FIVE_CONSEC_SC: message.FIVE_CONSEC_SC + 1,
-					});
-				}
-				break;
-			case 'FULL_ROUND_OF_SC':
-				if (reset) {
-					resetFullRoundOfSureCorrect();
-				} else {
+			switch (messageType) {
+				case 'FIVE_FAST_ANSWERS':
+					if (reset || message.FIVE_FAST_ANSWERS === 5) {
+						resetFiveFastAnswers();
+					} else {
+						setMessage({
+							...message,
+							FIVE_FAST_ANSWERS: message.FIVE_FAST_ANSWERS + 1,
+						});
+					}
+					break;
+				case 'FIVE_CONSEC_SI':
+					if (reset || message.FIVE_CONSEC_SI === 5) {
+						resetFiveConsecutiveSureIncorrect();
+					} else {
+						setMessage({
+							...message,
+							FIVE_CONSEC_SI: message.FIVE_CONSEC_SI + 1,
+						});
+					}
+					break;
+				case 'SIX_DK_IN_ROUND':
+					if (reset || message.SIX_DK_IN_ROUND === 6) {
+						resetSixDontKnowInRound();
+					} else {
+						setMessage({
+							...message,
+							SIX_DK_IN_ROUND: message.SIX_DK_IN_ROUND + 1,
+						});
+					}
+					break;
+				case 'FIVE_CONSEC_SC':
+					if (reset || message.FIVE_CONSEC_SC === 5) {
+						resetFiveConsecutiveSureCorrect();
+					} else {
+						setMessage({
+							...message,
+							FIVE_CONSEC_SC: message.FIVE_CONSEC_SC + 1,
+						});
+					}
+					break;
+				case 'FULL_ROUND_OF_SC':
+					if (reset) {
+						resetFullRoundOfSureCorrect();
+					} else {
+						setMessage({
+							...message,
+							FULL_ROUND_OF_SC: message.FULL_ROUND_OF_SC + 1,
+						});
+					}
+					break;
+				case 'BOTH_SC':
 					setMessage({
 						...message,
 						FULL_ROUND_OF_SC: message.FULL_ROUND_OF_SC + 1,
+						FIVE_CONSEC_SC: message.FIVE_CONSEC_SC + 1,
 					});
-				}
-				break;
-			case 'BOTH_SC':
-				setMessage({
-					...message,
-					FULL_ROUND_OF_SC: message.FULL_ROUND_OF_SC + 1,
-					FIVE_CONSEC_SC: message.FIVE_CONSEC_SC + 1,
-				});
-				break;
-			case 'FIVE_FAST_REVIEWS':
-				if (reset || message.FIVE_FAST_REVIEWS === 5) {
-					resetFiveFastReviews();
-				} else {
-					setMessage({
-						...message,
-						FIVE_FAST_REVIEWS: message.FIVE_FAST_REVIEWS + 1,
-					});
-				}
-				break;
-			case 'TEN_LONG_REVIEWS':
-				if (reset || message.TEN_LONG_REVIEWS === 10) {
-					resetTenLongReviews();
-				} else {
-					setMessage({
-						...message,
-						TEN_LONG_REVIEWS: message.TEN_LONG_REVIEWS + 1,
-					});
-				}
-				break;
-			default:
-				// handle default case
-				break;
-		}
-	};
+					break;
+				case 'FIVE_FAST_REVIEWS':
+					if (reset || message.FIVE_FAST_REVIEWS === 5) {
+						resetFiveFastReviews();
+					} else {
+						setMessage({
+							...message,
+							FIVE_FAST_REVIEWS: message.FIVE_FAST_REVIEWS + 1,
+						});
+					}
+					break;
+				case 'TEN_LONG_REVIEWS':
+					if (reset || message.TEN_LONG_REVIEWS === 10) {
+						resetTenLongReviews();
+					} else {
+						setMessage({
+							...message,
+							TEN_LONG_REVIEWS: message.TEN_LONG_REVIEWS + 1,
+						});
+					}
+					break;
+				default:
+					// handle default case
+					break;
+			}
+		},
+		[message],
+	);
 
 	const value = useMemo(
 		() => ({ message, handleMessage, selectedCourseKey, setSelectedCourseKey }),
