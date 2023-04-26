cd7507a68721e44ca67ec46ec6634c5928cd88c1
Jason Watkins <jwatkins@amplifire.com>
2023-04-26 11:44:39 -0400

    check if NPA is greater than or equal to 2

--- src/components/ui/Assignment/AssignmentComponent.tsx
+++ src/components/ui/Assignment/AssignmentComponent.tsx
@@ -447,7 +447,7 @@ export default function AssignmentComponent({
 			(obj) => obj.questionId === questionInFocus.publishedQuestionId,
 		);
 
-		if (message.TWO_NPA_ON_LU[index].npaCount >= 3) {
+		if (message.TWO_NPA_ON_LU[index].npaCount >= 2) {
 			setIsToastOpen(true);
 			setTextPrompt('TWO_NPA_ON_LU');
 			handleMessage(
