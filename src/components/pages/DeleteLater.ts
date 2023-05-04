eae17361ffb0855b4eac69939fcf9569d1af31da
Kush Shah <kshah@amplifire.com>
2023-05-04 15:24:48 -0600

    add example data

---
+++ src/components/pages/DeleteLater.ts
@@ -0,0 +1,47 @@
+export const example = {
+	FIVE_FAST_ANSWERS: 1,
+	FIVE_CONSEC_SI: 1,
+	SIX_DK_IN_ROUND: 0,
+	FIVE_CONSEC_SC: 0,
+	FULL_ROUND_OF_SC: 0,
+	FIVE_FAST_REVIEWS: 0,
+	TWO_FAST_REVIEWS_IN_LU: [
+		{
+			questionId: 0,
+			fastReviewsOnQuestion: 3,
+		},
+		{
+			questionId: 19609,
+			fastReviewsOnQuestion: 2,
+		},
+		{
+			questionId: 19602,
+			fastReviewsOnQuestion: 1,
+		},
+	],
+	TEN_LONG_REVIEWS: 1,
+	TWO_IDENTICAL_SI: [
+		{
+			questionId: 19612,
+			siCount: 1,
+			answerIdArray: [77040],
+		},
+		{
+			questionId: 19613,
+			siCount: 1,
+			answerIdArray: [77043],
+		},
+	],
+	TWO_NPA_ON_LU: [
+		{
+			questionId: 19612,
+			seenCount: 1,
+			npaCount: 0,
+		},
+		{
+			questionId: 19613,
+			seenCount: 1,
+			npaCount: 0,
+		},
+	],
+};
