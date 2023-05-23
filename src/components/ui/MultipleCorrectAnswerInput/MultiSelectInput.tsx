29009b9831b5f49796ab770a7c137034f5d827f0
Ryan Acevedo <racevedo@amplifire.com>
2023-05-23 15:37:48 -0600

    fixed color

--- src/components/ui/MultipleCorrectAnswerInput/MultiSelectInput.tsx
+++ src/components/ui/MultipleCorrectAnswerInput/MultiSelectInput.tsx
@@ -37,7 +37,7 @@ const MultiSelectInput = ({
 	};
 	return (
 		<Checkbox
-			borderColor={'black'}
+			borderColor={'#1e1f20'}
 			variant={'multiSelect'}
 			value={questionAnswerId}
 			isChecked={Boolean(isChecked)}
