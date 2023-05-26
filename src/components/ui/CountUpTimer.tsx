46e9dc21fb98dad15bfa5ca07d45cdf16d57f8cc
Jason Watkins <jwatkins@amplifire.com>
2023-05-26 09:13:41 -0400

    add ref to timer

--- src/components/ui/CountUpTimer.tsx
+++ src/components/ui/CountUpTimer.tsx
@@ -1,29 +1,34 @@
 import { Box, Text } from '@chakra-ui/react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const CountUpTimer = () => {
 	const [timeState, setTimeState] = useState([0, 0, 0]);
 	const [hours, minutes, seconds] = timeState;
-
+	const countUpRef = useRef<ReturnType<typeof setInterval>>();
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setTimeState((currentTime) => {
-				const [currentHours, currentMinutes, currentSeconds] = currentTime;
-				let newHours = currentHours,
-					newMinutes = currentMinutes,
-					newSeconds = currentSeconds + 1;
-				if (newSeconds >= 60) {
-					newMinutes += 1;
-					newSeconds = 0;
-				}
-				if (newMinutes >= 60) {
-					newHours += 1;
-					newMinutes = 0;
-				}
-				return [newHours, newMinutes, newSeconds];
-			});
-		}, 1000);
-		return () => clearInterval(interval);
+		const interval = () => {
+			countUpRef.current = setInterval(() => {
+				setTimeState((currentTime) => {
+					const [currentHours, currentMinutes, currentSeconds] = currentTime;
+					let newHours = currentHours,
+						newMinutes = currentMinutes,
+						newSeconds = currentSeconds + 1;
+					if (newSeconds >= 60) {
+						newMinutes += 1;
+						newSeconds = 0;
+					}
+					if (newMinutes >= 60) {
+						newHours += 1;
+						newMinutes = 0;
+					}
+					return [newHours, newMinutes, newSeconds];
+				});
+			}, 1000);
+		};
+
+		interval();
+
+		return () => clearInterval(countUpRef.current);
 	}, []);
 
 	return (
