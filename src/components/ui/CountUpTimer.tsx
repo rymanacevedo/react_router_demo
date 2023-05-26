899109ca100cb610bd2db91205854ee7fb9b8039
Jason Watkins <jwatkins@amplifire.com>
2023-05-26 13:24:36 -0400

    changed quotes to doublequotes

--- src/components/ui/CountUpTimer.tsx
+++ src/components/ui/CountUpTimer.tsx
@@ -9,7 +9,7 @@ type CountUpTimerProps = {
 const CountUpTimer = ({ hours, minutes, seconds }: CountUpTimerProps) => {
 	return (
 		<Box>
-			<Text fontSize={'21px'} fontWeight="600" color={'ampWhite'}>
+			<Text fontSize="21px" fontWeight="600" color="ampWhite">
 				{hours.toString().padStart(2, '0')}:
 				{minutes.toString().padStart(2, '0')}:
 				{seconds.toString().padStart(2, '0')}
