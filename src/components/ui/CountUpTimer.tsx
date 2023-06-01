6d8ed271bfc01499d0ba5d8c35af6a11f44ba4b0
Jason Watkins <jwatkins@amplifire.com>
2023-06-01 13:40:36 -0400

    extracted time calculation function to logic.tsx

--- src/components/ui/CountUpTimer.tsx
+++ src/components/ui/CountUpTimer.tsx
@@ -1,20 +1,11 @@
 import { Box, Text } from '@chakra-ui/react';
+import { convertSecondsToTime } from '../../utils/logic';
 
 type CountUpTimerProps = {
 	seconds: number;
 };
 
 const CountUpTimer = ({ seconds }: CountUpTimerProps) => {
-	const convertSecondsToTime = (timerSeconds: number): string => {
-		const hours = Math.floor(timerSeconds / 3600);
-		const minutes = Math.floor((timerSeconds - hours * 3600) / 60);
-		const convertedSeconds = timerSeconds - hours * 3600 - minutes * 60;
-
-		return `${hours.toString().padStart(2, '0')}:${minutes
-			.toString()
-			.padStart(2, '0')}:${convertedSeconds.toString().padStart(2, '0')}`;
-	};
-
 	return (
 		<Box>
 			<Text fontSize="21px" fontWeight="600" color="ampWhite">
