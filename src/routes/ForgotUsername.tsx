fcfaeb51c5b8f13f78f2eaff4c0c4498a5703679
Kush Shah <kshah@amplifire.com>
2023-06-14 12:35:08 -0600

    possible fix for handling json

--- src/routes/ForgotUsername.tsx
+++ src/routes/ForgotUsername.tsx
@@ -69,28 +69,45 @@ export const forgotUsernameAction: ActionFunction = async ({
 	}
 
 	const { data } = await getForgotUsernameData(fields);
-	const items = data.items;
 
-	if (items && items[0].message === 'email address not found') {
-		return badRequest({
-			fields,
-			errors: {
-				formErrors: ['email address not found'],
-				fieldsErrors: {
-					email: ['email address not found'],
-				},
-			},
-		});
-	}
+	if (data) {
+		if (data.items) {
+			const items = data.items;
 
-	if (items && items[0].message) {
-		return badRequest({
-			fields,
-			errors: {
-				formErrors: ['Uh oh, something went wrong'],
-				fieldErrors: {},
-			},
-		});
+			if (items && items[0].message === 'email address not found') {
+				return badRequest({
+					fields,
+					errors: {
+						formErrors: ['email address not found'],
+						fieldsErrors: {
+							email: ['email address not found'],
+						},
+					},
+				});
+			}
+
+			if (items && items[0].message === 'invalid email address') {
+				return badRequest({
+					fields,
+					errors: {
+						formErrors: ['invalid email address'],
+						fieldsErrors: {
+							email: ['invalid email address'],
+						},
+					},
+				});
+			}
+
+			if (items && items[0].message) {
+				return badRequest({
+					fields,
+					errors: {
+						formErrors: ['Uh oh, something went wrong'],
+						fieldErrors: {},
+					},
+				});
+			}
+		}
 	}
 
 	return redirect('/success?successMessageToShow=forgotUsername');
