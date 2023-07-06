1f6a8bc450e0855565f91c4c0c7c526e9947b6f5
Chris Wynn <cwynn@mojotech.com>
2023-07-06 09:40:17 -0400

    Display toast message when user copies or deletes authoring course

--- src/components/pages/AuthoringView.tsx
+++ src/components/pages/AuthoringView.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
 	Box,
 	Container,
@@ -7,6 +7,7 @@ import {
 	Button,
 	Grid,
 	GridItem,
+	useToast,
 } from '@chakra-ui/react';
 import { PlusIcon } from '@radix-ui/react-icons';
 import { ActionFunction, LoaderFunction } from 'react-router';
@@ -14,7 +15,7 @@ import { Cookies } from 'react-cookie-consent';
 import { requireUser } from '../../utils/user';
 import { getCourseList } from '../../services/authoring';
 import { getSubAccount } from '../../services/utils';
-import { json, useLoaderData } from 'react-router-dom';
+import { json, useLoaderData, useActionData } from 'react-router-dom';
 import CourseCard from '../ui/Authoring/CourseCard';
 import CourseFilter from '../ui/Authoring/CourseFilters';
 import { deleteCourse, copyCourse } from '../../services/authoring';
@@ -27,18 +28,40 @@ export const authoringActions: ActionFunction = async ({ request }) => {
 
 	if (intent === 'delete') {
 		const { response } = await deleteCourse(user, courseId);
+		const { status, statusText } = response;
 		if (response.status === 200) {
-			return json({ ok: true });
+			return json({
+				ok: true,
+				status,
+				statusText,
+				toastMessage: 'Course Deleted',
+			});
 		} else {
-			throw response;
+			return json({
+				ok: false,
+				status,
+				statusText,
+				toastMessage: 'Course Delete Failure',
+			});
 		}
 	} else if (intent === 'copyNew' || intent === 'copyShare') {
 		const share = intent === 'copyShare' ? true : false;
 		const { response } = await copyCourse(user, courseId, share);
-		if (response.status === 200) {
-			return json({ ok: true });
+		const { status, statusText } = response;
+		if (status === 200) {
+			return json({
+				ok: true,
+				status,
+				statusText,
+				toastMessage: 'Course Copied',
+			});
 		} else {
-			throw response;
+			return json({
+				ok: false,
+				status,
+				statusText,
+				toastMessage: 'Course Copy Failure',
+			});
 		}
 	}
 };
@@ -60,7 +83,9 @@ export const authoringLoader: LoaderFunction = async () => {
 };
 
 const AuthoringView = () => {
+	const actionData = useActionData() as any;
 	const { courseList } = useLoaderData() as any;
+	const toast = useToast();
 	const [listView, setListView] = useState<boolean>(
 		Boolean(Cookies.get('authoring_list_view')),
 	);
@@ -73,6 +98,24 @@ const AuthoringView = () => {
 			Cookies.set('authoring_list_view', 'true');
 		}
 	};
+
+	useEffect(() => {
+		if (actionData) {
+			if (actionData.status === 200) {
+				toast({
+					title: actionData.toastMessage,
+					status: 'success',
+					duration: 4000,
+				});
+			} else {
+				toast({
+					title: actionData.toastMessage,
+					status: 'error',
+					duration: 4000,
+				});
+			}
+		}
+	}, [actionData]);
 
 	return (
 		<Box bg="ampNeutral.100" minHeight="100vh" paddingX={6} paddingTop={6}>
