6817717359ea02df5932dc2fd581ff2431827364
Jason Watkins <jwatkins@amplifire.com>
2023-07-17 10:36:31 -0400

    further adjustments to shorten main app header to 60px and center children vertically

--- src/components/ui/CourseHome.jsx
+++ src/components/ui/CourseHome.jsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Link, Text } from '@chakra-ui/react';
+import { HStack, Icon, Link, Text, Center } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
@@ -10,20 +10,20 @@ const CourseHome = () => {
 	const { clearTimer } = useProgressMenuContext();
 
 	return (
-		<Link
-			as={ReactRouterLink}
-			to="/learning"
-			textDecoration="none"
-			w="full"
-			color="ampWhite"
-			h="80px"
-			py="25px"
-			onClick={clearTimer}>
-			<HStack>
-				<Icon as={ArrowLeftIcon} />
-				<Text>{i18n('courseHome')}</Text>
-			</HStack>
-		</Link>
+		<Center h="60px">
+			<Link
+				as={ReactRouterLink}
+				to="/learning"
+				textDecoration="none"
+				w="full"
+				color="ampWhite"
+				onClick={clearTimer}>
+				<HStack>
+					<Icon as={ArrowLeftIcon} />
+					<Text>{i18n('courseHome')}</Text>
+				</HStack>
+			</Link>
+		</Center>
 	);
 };
 
