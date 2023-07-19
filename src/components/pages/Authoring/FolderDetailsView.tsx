2a12a05b2714ceb795219c5112b105e0a184ea20
Chris Wynn <cwynn@mojotech.com>
2023-07-18 22:45:29 -0400

    Add optional breadcrumb to course filters navigation

--- src/components/pages/Authoring/FolderDetailsView.tsx
+++ src/components/pages/Authoring/FolderDetailsView.tsx
@@ -64,6 +64,7 @@ const FolderDetailsView = () => {
 						listView={listView}
 						handleListView={handleListFilter}
 						sortOrder={sortOrder}
+						breadCrumb={folderDetails.name}
 					/>
 				}
 			/>
