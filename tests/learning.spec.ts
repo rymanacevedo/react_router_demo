5d5d3decebf92c4cf1355e037d20dd55b6e66939
Ryan Acevedo <racevedo@amplifire.com>
2023-07-21 12:42:09 -0600

    added tests for clicking a course

--- tests/learning.spec.ts
+++ tests/learning.spec.ts
@@ -1,17 +1,40 @@
-import {test} from "@playwright/test";
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-   await page.goto('http://localhost:3000/main/login?abbrevName=automation_6');
-   await page.getByPlaceholder('name@email.com').fill('learner1');
-   await page.fill('[name="password"]', 'Welcome!');
-   await page.getByRole('button', { name: 'Log in' }).click();
+	await page.goto('http://localhost:3000/main/login?abbrevName=automation_10');
+	await page.getByPlaceholder('name@email.com').fill('learner1');
+	await page.fill('[name="password"]', 'Welcome!');
+	await page.getByRole('button', { name: 'Log in' }).click();
+	await page.waitForURL('http://localhost:3000/main/learning');
 });
 
 test.afterEach(async ({ page }) => {
-   await page.goto('http://localhost:3000/main/logout');
+	// await page.goto('http://localhost:3000/main/logout');
 });
 
-
 test('is on the dashboard', async ({ page }) => {
-   await page.waitForURL('http://localhost:3000/main/learning');
+	await page.waitForURL('http://localhost:3000/main/learning/PJBRRNAKW');
+	await page.waitForSelector('h1');
+	const h1Element = page.locator('h1');
+	const h1Text = await h1Element.innerText();
+	expect(h1Text).toBe('EightQuestionCourse');
+});
+
+test('can navigate to a course', async ({ page }) => {
+	//    assert that the course is displayed
+	await page.goto('http://localhost:3000/main/learning/8R4DRDLYN');
+	await page.waitForSelector('h1');
+	const h1Element = page.locator('h1');
+	const h1Text = await h1Element.innerText();
+	expect(h1Text).toBe('Math - Course 1');
+});
+
+test('can go to a course, then navigate to a lesson', async ({ page }) => {
+	await page.goto('http://localhost:3000/main/learning/2XSHRUSHM');
+	await page.waitForSelector('role=list');
+	await page.locator('li').nth(1).click();
+	await page.waitForSelector('h1');
+	const h1Element = page.locator('h1');
+	const h1Text = await h1Element.innerText();
+	expect(h1Text).toBe('Module: a new module');
 });
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,9 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('login', async ({ page }) => {
-   await page.goto('http://localhost:3000/main/login')
-   const h1Element = page.locator('h1');
-   const h1Text = await h1Element.innerText();
+	await page.goto('http://localhost:3000/main/login');
+	const h1Element = page.locator('h1');
+	const h1Text = await h1Element.innerText();
 
-   expect(h1Text).toBe("Welcome to Amplifire");
+	expect(h1Text).toBe('Welcome to Amplifire');
 });
