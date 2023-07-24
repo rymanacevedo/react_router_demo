01a2385cb6566e6aacc99d3ef24a7f6ecc1bf30f
Andrew Gilmartin <agilmartin@mojotech.com>
2023-07-24 09:11:51 -0400

    feat: Add simple mechanism for maintaining and using cookies.

---
+++ src/lib/authoring/cookies.ts
@@ -0,0 +1,37 @@
+import { Cookies } from 'react-cookie-consent';
+
+/**
+ * Returns a tuple containing a function to get the current cookie value (or default if unset)
+ * and a function to set the cookie value (or remove it if null or the default value).
+ *
+ * @param key the cookie's name
+ * @param defaultValue the cookie's default value
+ * @param options See https://github.com/js-cookie/js-cookie
+ */
+const handleCookie = (key: string, defaultValue: string, options = {}) =>
+	[
+		() => {
+			return (Cookies.get(key) as string) || defaultValue;
+		},
+		(value: string) => {
+			if (value === null || value == defaultValue) {
+				Cookies.remove(key);
+			} else {
+				Cookies.set(key, value, options);
+			}
+		},
+	] as const;
+
+// Preferred course content list sort order
+export const [courseContentsOrder, setCourseContentsOrder] = handleCookie(
+	'amp-course-contents-order',
+	'a',
+);
+
+// Preferred folder course content list sort order
+export const [folderContentsOrder, setFolderContentsOrder] = handleCookie(
+	'amp-folder-contents-order',
+	'a',
+);
+
+// END
