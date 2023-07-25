875150551472c78f1ca639bab442d435717bb809
Ryan Acevedo <racevedo@amplifire.com>
2023-07-25 11:45:25 -0600

    adjusted test to pass

--- src/components/ui/TimedAssessment/AmpBoxWithQuestionAndAnswer.tsx
+++ src/components/ui/TimedAssessment/AmpBoxWithQuestionAndAnswer.tsx
@@ -190,7 +190,7 @@ export default function AmpBoxWithQuestionAndAnswer() {
 
 			setSelectedAnswer(iSa);
 		} else {
-			setQuestionInFocus(null);
+			setQuestionInFocus(question);
 			navigate(`/learning/timedAssessment/${assignmentUid}/submission`);
 		}
 	};
--- tests/learning.timedassessment.spec.ts
+++ tests/learning.timedassessment.spec.ts
@@ -1,19 +1,23 @@
-import {test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-   await page.goto('http://localhost:3000/main/login?abbrevName=automation_6');
-   await page.getByPlaceholder('name@email.com').fill('learner1');
-   await page.fill('[name="password"]', 'Welcome!');
-   await page.getByRole('button', { name: 'Log in' }).click();
-   await page.waitForURL('http://localhost:3000/main/learning');
+	await page.goto('http://localhost:3000/main/login?abbrevName=automation_6');
+	await page.getByPlaceholder('name@email.com').fill('learner1');
+	await page.fill('[name="password"]', 'Welcome!');
+	await page.getByRole('button', { name: 'Log in' }).click();
+	await page.waitForURL('http://localhost:3000/main/learning');
 });
 
-
-test('can navigate to the review and submit page' , async ({ page }) => {
-   await page.goto('http://localhost:3000/main/learning/AHWCREU2G');
-   await page.waitForSelector('role=list');
-   await page.locator('li').nth(1).click();
-   await page.waitForSelector('role=button');
-   await page.getByRole('button', { name: 'Finish practice test' }).click();
-// assert we are on the submit page
+test('can navigate to the review and submit page', async ({ page }) => {
+	await page.goto('http://localhost:3000/main/learning/AHWCREU2G');
+	await page.waitForSelector('[role=list]');
+	await page.locator('li').nth(1).click();
+	await page.waitForURL(
+		'http://localhost:3000/main/learning/timedAssessment/f2c0cef1-e4eb-44ae-9dd9-ce3899724194/147824',
+	);
+	await page.getByRole('button', { name: 'Finish practice test' }).click();
+	await page.waitForURL(
+		'http://localhost:3000/main/learning/timedAssessment/f2c0cef1-e4eb-44ae-9dd9-ce3899724194/submission',
+	);
+	await expect(page.getByText('Review & Submit')).toBeVisible();
 });
