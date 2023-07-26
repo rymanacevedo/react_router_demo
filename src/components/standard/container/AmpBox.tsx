f26f778b0e70fbe287ab6435e9c395203d1bfc0b
Ryan Acevedo <racevedo@amplifire.com>
2023-07-26 11:11:59 -0600

    refined gold standard AmpBox to flex grow to parent

--- src/components/standard/container/AmpBox.tsx
+++ src/components/standard/container/AmpBox.tsx
@@ -1,16 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
 export default function AmpBox({ children }: { children?: ReactNode }) {
 	return (
-		<Box
+		<Flex
 			backgroundColor="ampWhite"
 			boxShadow="md"
-			borderRadius={24}
+			borderRadius="3xl"
 			px={18}
 			py={11}
-			w={{ base: '100%', md: '50%' }}>
+			flex={1}
+			direction="column">
 			{children}
-		</Box>
+		</Flex>
 	);
 }
