d0443b2c2e976110d18b8947a082f45f1f402dde
Chris Wynn <cwynn@mojotech.com>
2023-07-26 22:27:24 -0400

    Add course link to course card

--- src/components/ui/Authoring/CourseCard.tsx
+++ src/components/ui/Authoring/CourseCard.tsx
@@ -8,6 +8,8 @@ import {
 	Heading,
 	useTheme,
 	Checkbox,
+	LinkBox,
+	LinkOverlay,
 } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import {
@@ -16,6 +18,7 @@ import {
 	BellIcon,
 	DotFilledIcon,
 } from '@radix-ui/react-icons';
+import { Link as RouterLink } from 'react-router-dom';
 import { formatDate } from '../../../lib/utils';
 import CourseCardDropdownMenu from './CourseCardDropdownMenu';
 import { CourseContent } from '../../../store/slices/authoring/coursesViewSlice';
@@ -79,108 +82,116 @@ const CourseCard = ({
 		: {};
 
 	return (
-		<Card
-			variant="authoringCard"
-			direction={listView ? 'row' : 'column'}
-			justifyContent="space-between"
-			role="group"
-			height="100%">
-			<CardHeader
-				width="100%"
-				marginBottom={listView ? 0 : 4}
-				maxWidth={listView ? 500 : 'auto'}>
-				<Flex
+		<LinkBox>
+			<Card
+				variant="authoringCard"
+				direction={listView ? 'row' : 'column'}
+				justifyContent="space-between"
+				role="group"
+				height="100%">
+				<CardHeader
 					width="100%"
-					direction={listView ? 'row' : 'column'}
-					gap={4}
-					position="relative">
+					marginBottom={listView ? 0 : 4}
+					maxWidth={listView ? 500 : 'auto'}>
 					<Flex
-						alignItems="center"
-						justifyContent="space-between"
+						width="100%"
+						direction={listView ? 'row' : 'column'}
+						gap={4}
 						position="relative">
-						<Flex gap={3}>
-							{bulkEditingEnabled ? (
-								<Checkbox value={uid} variant="formCheckbox" />
-							) : null}
-							<Badge
-								variant={statusBadgeVariant[status]}
-								textTransform="capitalize">
-								{status}
-							</Badge>
-						</Flex>
-						{!listView && !bulkEditingEnabled && (
-							<CourseCardDropdownMenu
-								uid={uid}
-								isPublished={isPublished}
-								folderUid={folderUid}
-							/>
-						)}
-					</Flex>
-					<Heading
-						fontWeight="semibold"
-						fontSize="lg"
-						as="h2"
-						noOfLines={listView ? 1 : 2}>
-						{name}
-					</Heading>
-				</Flex>
-			</CardHeader>
-			<CardBody width={listView ? 'auto' : '100%'}>
-				<Flex
-					justifyContent={'space-between'}
-					height="100%"
-					alignItems="flex-end">
-					<Flex
-						height="100%"
-						alignItems="flex-start"
-						flexDirection={listView ? 'row' : 'column'}
-						justifyContent="flex-end"
-						gap={listView ? 1 : 0}>
 						<Flex
-							marginBottom={listView ? 0 : 2}
-							marginRight={listView ? 5 : 0}>
-							<Text fontWeight="normal" fontSize="xs">
-								{moduleCount} Modules
+							alignItems="center"
+							justifyContent="space-between"
+							position="relative">
+							<Flex gap={3}>
+								{bulkEditingEnabled ? (
+									<Checkbox value={uid} variant="formCheckbox" />
+								) : null}
+								<Badge
+									variant={statusBadgeVariant[status]}
+									textTransform="capitalize">
+									{status}
+								</Badge>
+							</Flex>
+							{!listView && !bulkEditingEnabled && (
+								<CourseCardDropdownMenu
+									uid={uid}
+									isPublished={isPublished}
+									folderUid={folderUid}
+								/>
+							)}
+						</Flex>
+						<LinkOverlay
+							as={RouterLink}
+							to={`/authoring/course/${uid}`}
+							textDecoration="none"
+							color="ampPrimaryText">
+							<Heading
+								fontWeight="semibold"
+								fontSize="lg"
+								as="h2"
+								noOfLines={listView ? 1 : 2}>
+								{name}
+							</Heading>
+						</LinkOverlay>
+					</Flex>
+				</CardHeader>
+				<CardBody width={listView ? 'auto' : '100%'}>
+					<Flex
+						justifyContent={'space-between'}
+						height="100%"
+						alignItems="flex-end">
+						<Flex
+							height="100%"
+							alignItems="flex-start"
+							flexDirection={listView ? 'row' : 'column'}
+							justifyContent="flex-end"
+							gap={listView ? 1 : 0}>
+							<Flex
+								marginBottom={listView ? 0 : 2}
+								marginRight={listView ? 5 : 0}>
+								<Text fontWeight="normal" fontSize="xs">
+									{moduleCount} Modules
+								</Text>
+								<DotFilledIcon color={colors.ampNeutral[300]} />
+								<Text fontWeight="normal" fontSize="xs">
+									{learningUnitCount} Questions
+								</Text>
+							</Flex>
+							<Text color="ampTertiaryText" fontSize="xs" fontWeight="normal">
+								Last Edited {month} {day}, {year}
 							</Text>
-							<DotFilledIcon color={colors.ampNeutral[300]} />
-							<Text fontWeight="normal" fontSize="xs">
-								{learningUnitCount} Questions
+							<Text color="ampTertiaryText" fontSize="xs" fontWeight="normal">
+								by {modifiedUserFullName}
 							</Text>
 						</Flex>
-						<Text color="ampTertiaryText" fontSize="xs" fontWeight="normal">
-							Last Edited {month} {day}, {year}
-						</Text>
-						<Text color="ampTertiaryText" fontSize="xs" fontWeight="normal">
-							by {modifiedUserFullName}
-						</Text>
+						<Flex
+							alignContent="flex-end"
+							position="relative"
+							gap={listView ? 2 : 0}>
+							<Badge variant={alertBadgeVariant.variant} display="flex" gap={2}>
+								{alertBadgeVariant.icon}
+								<Text
+									as="span"
+									fontSize="inherit"
+									position="absolute"
+									opacity={0}
+									color="inherit"
+									_groupHover={{
+										opacity: 1,
+										position: 'relative',
+										transition: '.6s ease',
+									}}>
+									{alertBadgeVariant.label}
+								</Text>
+							</Badge>
+							{listView && !bulkEditingEnabled && (
+								<CourseCardDropdownMenu uid={uid} isPublished={isPublished} />
+							)}
+						</Flex>
 					</Flex>
-					<Flex
-						alignContent="flex-end"
-						position="relative"
-						gap={listView ? 2 : 0}>
-						<Badge variant={alertBadgeVariant.variant} display="flex" gap={2}>
-							{alertBadgeVariant.icon}
-							<Text
-								as="span"
-								fontSize="inherit"
-								position="absolute"
-								opacity={0}
-								color="inherit"
-								_groupHover={{
-									opacity: 1,
-									position: 'relative',
-									transition: '.6s ease',
-								}}>
-								{alertBadgeVariant.label}
-							</Text>
-						</Badge>
-						{listView && !bulkEditingEnabled && (
-							<CourseCardDropdownMenu uid={uid} isPublished={isPublished} />
-						)}
-					</Flex>
-				</Flex>
-			</CardBody>
-		</Card>
+				</CardBody>
+			</Card>
+		</LinkBox>
 	);
 };
 
