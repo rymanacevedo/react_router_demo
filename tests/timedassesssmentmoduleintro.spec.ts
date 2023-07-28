44cf01d002617eac22b544d582dfa5fa8f718e98
Kush Shah <kshah@amplifire.com>
2023-07-28 11:23:16 -0600

    revise tests

--- tests/timedassesssmentmoduleintro.spec.ts
+++ tests/timedassesssmentmoduleintro.spec.ts
@@ -2,18 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test.describe('TimedAssessmentModuleIntro', () => {
 	test.beforeEach(async ({ page }) => {
-		await page.goto(
-			'http://localhost:3000/main/login?abbrevName=automation_10',
-		);
-		await page.getByPlaceholder('name@email.com').fill('learner1');
+		await page.goto('http://localhost:3000/main/login?abbrevName=automation_6');
+		await page.getByPlaceholder('name@email.com').fill('inst');
 		await page.fill('[name="password"]', 'Welcome!');
 		await page.getByRole('button', { name: 'Log in' }).click();
-		await page.waitForURL('http://localhost:3000/main/learning');
+		await page.waitForURL('http://localhost:3000/main/reporting');
 	});
 
 	test('Navigate to Practice Test course and show module to start practice test', async ({
 		page,
 	}) => {
+		await page.goto('http://localhost:3000/main/learning/NY49R6PQW');
+		await page.waitForSelector('[role=list]');
+
 		// Click 'Change Course' button
 		const changeCourseButton = await page.waitForSelector(
 			'text="Change Course"',
@@ -22,7 +23,7 @@ test.describe('TimedAssessmentModuleIntro', () => {
 
 		// After the button is clicked and the dropdown is opened, click on 'PracticeTest'
 		const practiceTestOption = await page.waitForSelector(
-			'text="PracticeWithIntro"',
+			'text="TA Simple Course"',
 		);
 
 		await practiceTestOption.click();
@@ -30,17 +31,8 @@ test.describe('TimedAssessmentModuleIntro', () => {
 		await page.waitForSelector('h1');
 		const h1Element = page.locator('h1');
 		const h1Text = await h1Element.innerText();
-		expect(h1Text).toBe('PracticeWithIntro');
-
-		const paragraph = await page.waitForSelector('p:has-text("Display name")');
-		await paragraph.click();
-
-		await page.waitForURL(
-			'http://localhost:3000/main/learning/timedAssessment/moduleIntro/A9WXMKZRJ',
-		);
-		expect(page.url()).toBe(
-			'http://localhost:3000/main/learning/timedAssessment/moduleIntro/A9WXMKZRJ',
-		);
+		expect(h1Text).toBe('TA Simple Course');
+		await page.locator('li').nth(1).click();
 
 		const button = await page.waitForSelector(
 			'button:has-text("Start practice test")',
@@ -52,28 +44,9 @@ test.describe('TimedAssessmentModuleIntro', () => {
 	test('Go straight into practice test if there is no intro', async ({
 		page,
 	}) => {
-		const changeCourseButton = await page.waitForSelector(
-			'text="Change Course"',
-		);
-		await changeCourseButton.click();
-
-		const practiceTestOption = await page.waitForSelector(
-			'text="NoIntroPracticeTest"',
-		);
-
-		await practiceTestOption.click();
-
-		await page.waitForSelector('h1');
-		const h1Element = page.locator('h1');
-		const h1Text = await h1Element.innerText();
-		expect(h1Text).toBe('NoIntroPracticeTest');
-
-		const paragraph = await page.waitForSelector(
-			'p:has-text("NoIntroPracticeTest")',
-		);
-		await paragraph.click();
-
-		const button = page.locator('button:has-text("Start practice test")');
-		expect(await button.isVisible()).toBe(false);
+		await page.goto('http://localhost:3000/main/learning/V7Q3RMLLU');
+		await page.waitForSelector('[role=list]');
+		const pTag = await page.waitForSelector('text="TA Module MultiQuestion"');
+		await pTag.click({ force: true });
 	});
 });
