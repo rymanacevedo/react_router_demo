3a294182bb79335e51192680944dbd746dd6ce2a
Ryan Acevedo <racevedo@amplifire.com>
2023-08-01 12:07:11 -0600

    example of how to use useEffectOnce

--- src/routes/Authenticate.tsx
+++ src/routes/Authenticate.tsx
@@ -1,13 +1,14 @@
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { useNavigate, useRouteLoaderData } from 'react-router-dom';
 import { AuthenticationData } from '../App';
+import useEffectOnce from '../hooks/useEffectOnce';
 
 export default function Authenticate() {
 	const navigate = useNavigate();
 	const { redirectTo } = useRouteLoaderData('root') as AuthenticationData;
-	useEffect(() => {
+	useEffectOnce(() => {
 		navigate(`/${redirectTo}`);
-	}, []);
+	});
 	return (
 		<Suspense fallback={<h1>Loading...</h1>}>
 			{/*TODO: add an error component from Remix in case auth fails*/}
