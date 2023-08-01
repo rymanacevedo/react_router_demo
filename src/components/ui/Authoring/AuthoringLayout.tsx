92bc36c11c96022bf626d2ea1b26e3b146710f00
Andrew Gilmartin <agilmartin@mojotech.com>
2023-08-01 09:11:11 -0400

    feat: pages with navigation header format the header's container differently.

--- src/components/ui/Authoring/AuthoringLayout.tsx
+++ src/components/ui/Authoring/AuthoringLayout.tsx
@@ -1,22 +1,36 @@
-import { Children, PropsWithChildren } from 'react';
+import { Children, PropsWithChildren, ReactNode } from 'react';
 import { Box, Container, VStack } from '@chakra-ui/react';
 import ScrollToTop from './ScrollToTop';
 
 interface AuthoringLayoutProps {
 	stacked?: boolean;
+	hasNavigation?: ReactNode; // ie, the first child is to be formatted differently
 }
 
 const AuthoringLayout = ({
 	stacked = false,
+	hasNavigation = false,
 	children,
 }: PropsWithChildren<AuthoringLayoutProps>) => {
+	let stack = Children.toArray(children);
+	let navigation = hasNavigation && stack.shift();
 	return (
 		<>
 			<ScrollToTop />
 			<Box bg="ampNeutral.100" minHeight="100vh" paddingX="6" paddingY="6">
 				{stacked ? (
 					<VStack gap="6">
-						{Children.map(children, (child) => {
+						{navigation && (
+							<Container
+								maxW="1440"
+								bg="ampWhite"
+								borderRadius="xl"
+								paddingY={4}
+								paddingX={6}>
+								{navigation}
+							</Container>
+						)}
+						{stack.map((child) => {
 							return (
 								<Container
 									maxW="1440"
@@ -30,14 +44,26 @@ const AuthoringLayout = ({
 						})}
 					</VStack>
 				) : (
-					<Container
-						maxW="1440"
-						bg="ampWhite"
-						borderRadius="xl"
-						paddingY="16"
-						paddingX="24">
-						{children}
-					</Container>
+					<>
+						{navigation && (
+							<Container
+								maxW="1440"
+								bg="ampWhite"
+								borderRadius="xl"
+								paddingY={4}
+								paddingX={6}>
+								{navigation}
+							</Container>
+						)}
+						<Container
+							maxW="1440"
+							bg="ampWhite"
+							borderRadius="xl"
+							paddingY="16"
+							paddingX="24">
+							{stack}
+						</Container>
+					</>
 				)}
 			</Box>
 		</>
